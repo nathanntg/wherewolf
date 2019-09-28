c498f1143ef61738bd93e85a34c4ee3d80df528a
Nathan Perkins <nathan@maxmo.net>
2019-09-27 23:57:25 -0700

    Refactor as ES6 module

--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
-var Wherewolf = require("../wherewolf.js"),
-    fs = require("fs"),
-    path = require("path"),
-    assert = require("assert");
+import Wherewolf from "../wherewolf";
+import * as fs from "fs";
+import * as path from "path";
+import * as assert from "assert";
 
-var ww;
-var point = [-74.005363,40.726760]; //wnyc office
-//options, point style, type of file
+let ww;
+let point = [-74.005363,40.726760]; //wnyc office
 
-//check topojson add
-fs.readFile(path.join(__dirname,"../examples/nyc/districts.json"), "utf8", function(err, data){
-  //console.log(data);
+// check topojson add
+fs.readFile(path.join(__dirname,"../examples/nyc/districts.json"), "utf8", function(err, data) {
   data = JSON.parse(data);
-    ww = Wherewolf();
-    ww.addAll(data);
+
+  ww = new Wherewolf({index: false});
+  ww.addAll(data);
 
   assert.deepEqual(ww.layerNames().length, 9, ["wrong number of layers"]);
   assert.deepEqual(ww.remove("Police Precinct (NYC)").layerNames().length, 8, ["remove error"]);
@@ -25,8 +24,8 @@ fs.readFile(path.join(__dirname,"../examples/nyc/districts.json"), "utf8", funct
   assert.deepEqual(ww.find(point,{wholeFeature: true, layer: "Borough (NYC)"}).type, "Feature", ["whole feature + layer search error"]);
 
   // repeat with indexing enabled
-    ww = Wherewolf({index: true});
-    ww.addAll(data);
+  ww = new Wherewolf({index: true});
+  ww.addAll(data);
 
   assert.deepEqual(ww.find({lat:point[1], lng:point[0]})["Borough (NYC)"].name,"Manhattan", ["find dict point error"]);
   assert.deepEqual(ww.find(point)["Borough (NYC)"].name,"Manhattan", ["find array point error"]);
@@ -37,12 +36,12 @@ fs.readFile(path.join(__dirname,"../examples/nyc/districts.json"), "utf8", funct
   assert.deepEqual(ww.find(point,{wholeFeature: true, layer: "Borough (NYC)"}).type, "Feature", ["whole feature + layer search error"]);
 });
 
-//check geojson add
-fs.readFile(path.join(__dirname,"carlsjr_hardees/carlsjr_hardees.json"), "utf8", function(err, data){
-
+// check geojson add
+fs.readFile(path.join(__dirname,"carlsjr_hardees/carlsjr_hardees.json"), "utf8", function(err, data) {
   data = JSON.parse(data);
-    ww = Wherewolf();
-    ww.add("carlsjr_hardees", data);
+
+  ww = new Wherewolf();
+  ww.add("carlsjr_hardees", data);
 
   assert.deepEqual(ww.layerNames().length, 1, ["wrong number of layers"]);
   assert.deepEqual(ww.remove("carlsjr_hardees").layerNames().length, 0, ["remove error"]);
@@ -57,25 +56,23 @@ fs.readFile(path.join(__dirname,"carlsjr_hardees/carlsjr_hardees.json"), "utf8",
 
 });
 
-//check point add
-fs.readFile(path.join(__dirname,"point.geojson"), "utf8", function(err, data){
-
+// check point add
+fs.readFile(path.join(__dirname,"point.geojson"), "utf8", function(err, data) {
   data = JSON.parse(data);
-    ww = Wherewolf();
+  ww = Wherewolf();
 
-    var lName = "wnycoffice";
-    ww.add(lName, data, lName);
+  let layerName = "wnycoffice";
+  ww.add(layerName, data, layerName);
 
   assert.deepEqual(ww.layerNames().length, 1, ["wrong number of layers"]);
-  assert.deepEqual(ww.remove(lName).layerNames().length, 0, ["remove error"]);
-  ww.add(lName, data);
+  assert.deepEqual(ww.remove(layerName).layerNames().length, 0, ["remove error"]);
+  ww.add(layerName, data);
 
-  assert.deepEqual(ww.find({lat:point[1], lng:point[0]})[lName].name,"office", ["find dict point error"]);
-  assert.deepEqual(ww.find(point)[lName].name,"office", ["find array point error"]);
+  assert.deepEqual(ww.find({lat: point[1], lng: point[0]})[layerName].name, "office", ["find dict point error"]);
+  assert.deepEqual(ww.find(point)[layerName].name, "office", ["find array point error"]);
 
   //find options
-  assert.deepEqual(ww.find(point,{layer: "wnycoffice"}).name,"office", ["layer option search error"]);
-  assert.deepEqual(ww.find(point,{wholeFeature: true})[lName].type, "Feature", ["whole feature search error"]);
-  assert.deepEqual(ww.find(point,{wholeFeature: true, layer: lName}).type, "Feature", ["whole feature + layer search error"]);
-
+  assert.deepEqual(ww.find(point, {layer: "wnycoffice"}).name, "office", ["layer option search error"]);
+  assert.deepEqual(ww.find(point, {wholeFeature: true})[layerName].type, "Feature", ["whole feature search error"]);
+  assert.deepEqual(ww.find(point, {wholeFeature: true, layer: layerName}).type, "Feature", ["whole feature + layer search error"]);
 });
